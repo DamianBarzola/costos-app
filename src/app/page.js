197fe9f4e24de9083b76abf38b8bb50b404b3c65
DamianBarzola <dbdamix@gmail.com>
2024-09-26 19:18:17 -0300

    add input onchange and total

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,42 @@
 "use client";
 import { Navigator } from "@/components/Navigator.jsx";
 import { Box, MenuItem, TextField } from "@mui/material";
-const Data = [
+import { ELEMENT_TYPES } from "@/consts";
+import { useEffect } from "react";
+import { useState } from "react";
+
+let Data = [
   {
     id: 1,
     label: "Tela",
-    type: "select",
+    type: ELEMENT_TYPES.SELECT,
     amount: 1,
-    price: null,
+    // price: null,
     subtotal: null,
-    optionSelected: null,
+    optionSelected: "",
     options: [
       { id: 1, label: "Tela 1", price: 500 },
       { id: 2, label: "Tela 2", price: 500 },
     ],
   },
-  { id: 2, label: "Volado", type: "price", price: null, subtotal: null },
-  { id: 3, label: "Cinta", type: "price", price: null, subtotal: null },
+  {
+    id: 2,
+    label: "Volado",
+    type: ELEMENT_TYPES.PRICE,
+    price: 0,
+    subtotal: null,
+  },
+  {
+    id: 3,
+    label: "Cinta",
+    type: ELEMENT_TYPES.PRICE,
+    price: 0,
+    subtotal: null,
+  },
   {
     id: 4,
     label: "Etiqueta Eco",
-    type: "quantity",
+    type: ELEMENT_TYPES.QUANTITY,
     price: 150,
     amount: 1,
     subtotal: null,
@@ -28,16 +44,22 @@ const Data = [
   {
     id: 5,
     label: "Etiqueta",
-    type: "quantity",
+    type: ELEMENT_TYPES.QUANTITY,
     price: 50,
     amount: 1,
     subtotal: null,
   },
-  { id: 6, label: "Hilos", type: "price", price: null, subtotal: null },
+  {
+    id: 6,
+    label: "Hilos",
+    type: ELEMENT_TYPES.PRICE,
+    price: 0,
+    subtotal: null,
+  },
   {
     id: 7,
     label: "Mano de obra",
-    type: "quantity",
+    type: ELEMENT_TYPES.QUANTITY,
     price: 1800,
     amount: 1,
     subtotal: null,
@@ -45,37 +67,32 @@ const Data = [
   {
     id: 8,
     label: "Bolsa",
-    type: "quantity",
+    type: ELEMENT_TYPES.QUANTITY,
     price: 50,
     amount: 1,
     subtotal: null,
   },
 ];
 
-function RowField({ rowData }) {
-  const handleSelect = (e) => {
-    rowData.optionSelected = rowData.options.find(
-      (item) => item.id == e.target.value
-    );
-    rowData.subtotal = rowData.amount * rowData.optionSelected?.price;
-  };
-  const handleChange = (e) => {
-    rowData.amount = e.target.value;
-    rowData.subtotal = rowData.amount * rowData.optionSelected?.price;
-  };
+let revenue = 50;
+
+function RowField({ rowData, handleSelect, handleChange }) {
   return (
     <div className="flex justify-between gap-x-2">
       <div className="w-4/5">
-        {rowData.type === "select" ? (
+        {rowData.type === ELEMENT_TYPES.SELECT ? (
           <div className="flex justify-around gap-x-3 w-100">
             <TextField
               select
               variant="outlined"
               fullWidth
               label={rowData.label}
-              value={rowData.optionSelected?.id}
+              value={rowData.optionSelected.id ?? ""}
               onChange={handleSelect}
             >
+              <MenuItem value="" disabled>
+                Seleccione una opción
+              </MenuItem>
               {rowData.options.map((item) => (
                 <MenuItem key={item.id} value={item.id}>
                   {item.label + " ($" + item.price + ")"}
@@ -89,34 +106,94 @@ function RowField({ rowData }) {
               onChange={handleChange}
               value={rowData.amount}
               fullWidth
+              min={0}
             />
           </div>
-        ) : rowData.type === "quantity" ? (
+        ) : rowData.type === ELEMENT_TYPES.QUANTITY ? (
           <TextField
             label={rowData.label + " ($" + rowData.price + ")"}
             variant="outlined"
             type="number"
+            onChange={handleChange}
+            value={rowData.amount}
             fullWidth
+            min={0}
           />
         ) : (
-          rowData.type === "price" && (
+          rowData.type === ELEMENT_TYPES.PRICE && (
             <TextField
               label={rowData.label}
               variant="outlined"
               type="number"
+              onChange={handleChange}
+              min={0}
+              value={rowData.price}
               fullWidth
             />
           )
         )}
       </div>
       <div className="w-1/5 flex justify-center items-center">
-        <span>{rowData.subtotal ?? "-"}</span>
+        <span>{rowData.subtotal ? "$ " + rowData.subtotal : "-"}</span>
       </div>
     </div>
   );
 }
 
 export default function Home() {
+  const [data, setData] = useState(Data);
+
+  useEffect(() => {
+    let mappedSubtotal = Data.map((item) => {
+      if (item.type === ELEMENT_TYPES.SELECT) {
+        item.subtotal = 0;
+      } else if (item.type === ELEMENT_TYPES.QUANTITY) {
+        item.subtotal = item.amount * item.price;
+      } else if (item.type === ELEMENT_TYPES.PRICE) {
+        item.subtotal = item.price;
+      }
+      return item;
+    });
+
+    setData(mappedSubtotal);
+  }, []);
+
+  const handleSelect = (e, index) => {
+    const value = Number(e.target.value);
+    let row = data[index];
+    row.optionSelected = row.options.find((item) => item.id == value);
+    row.subtotal = row.amount * row.optionSelected?.price;
+    setData([...data]);
+  };
+
+  const handleChange = (e, index) => {
+    const value = Number(e.target.value);
+
+    if (value >= 0) {
+      let row = data[index];
+      if (row.type === ELEMENT_TYPES.SELECT) {
+        row.amount = value;
+        row.subtotal = row.amount * row.optionSelected?.price;
+      } else if (row.type === ELEMENT_TYPES.QUANTITY) {
+        row.amount = value;
+        row.subtotal = row.amount * row.price;
+      } else if (row.type === ELEMENT_TYPES.PRICE) {
+        row.price = value;
+
+        row.subtotal = row.price;
+      }
+      setData([...data]);
+    }
+  };
+  console.log(data);
+  const total = data.reduce(
+    (acc, item) => Number(acc) + (Number(item.subtotal) ?? 0),
+    0
+  );
+
+  const revenueAmount = (total * revenue) / 100;
+
+  const finalAmount = total + revenueAmount;
   return (
     <div className="flex justify-center min-h-screen">
       <main className="w-full max-w-[750px] px-4 sm:px-6 lg:px-8">
@@ -136,9 +213,28 @@ export default function Home() {
             margin: "0 auto",
           }}
         >
-          {Data.map((item) => (
-            <RowField rowData={item} key={item.id} />
+          {data.map((item, index) => (
+            <RowField
+              rowData={item}
+              key={item.id}
+              handleChange={(e) => handleChange(e, index)}
+              handleSelect={(e) => handleSelect(e, index)}
+            />
           ))}
+          <hr />
+          <div style={{ fontSize: "18px", fontWeight: "bold" }}>
+            <div className="flex justify-end px-2 mb-2">
+              <h6>Total sin ganancia: $ {total.toFixed(2)}</h6>
+            </div>
+            <div className="flex justify-end px-2 mb-2">
+              <h6>
+                Ganancia del {revenue}%: $ {revenueAmount.toFixed(2)}
+              </h6>
+            </div>
+            <div className="flex justify-end px-2 mb-2">
+              <h6>Total : $ {finalAmount.toFixed(2)}</h6>
+            </div>
+          </div>
         </Box>
         <Navigator />
       </main>
---
+++ src/consts.js
@@ -0,0 +1,5 @@
+export const ELEMENT_TYPES = {
+  SELECT: "select",
+  PRICE: "price",
+  QUANTITY: "quantity",
+};
